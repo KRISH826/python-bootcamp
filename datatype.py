e1e5b5a1e763ccad56ee95e8af77a2a3e885abd3
KRISH826 <krishnendupanja98@gmail.com>
2026-06-22 00:56:54 +0530

    Add datatype.py with basic data type definitions and example usage

---
+++ datatype.py
@@ -0,0 +1,11 @@
+## we have 2 types basic data type and advanced data type
+## basic data type
+## int float complex bool str bytes bytearray memoryview
+
+# we have integer, float, boolean, string datatypes
+
+result="hello" + str(10)
+print(result)
+
+upperResult = result.upper()
+print(upperResult)
