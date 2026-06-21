7a95499c7ea63717c3bf5f4ef3e298a50bddfca9
KRISH826 <krishnendupanja98@gmail.com>
2026-06-22 00:40:41 +0530

    Add initial implementation of index.py with variable definitions and type checks

---
+++ index.py
@@ -0,0 +1,34 @@
+name='krishnendu'
+Name='Panja'
+print(name)
+print(Name)
+
+print(type(name))
+print(type(Name))
+
+if name==Name:
+    print('both are same')
+else:
+    print('both are not same')    
+
+#variables
+
+a=10
+firstName='krishnendu'
+age=25
+projects= '25'
+height=5.6
+is_student=True
+strage=str(age)
+intProject = int(projects)
+
+print(type(strage))
+print(type(intProject))
+print(type(height))
+
+# dynamic typing
+_int=10
+print(_int,type(_int))
+
+_int='krishnendu'
+print(_int,type(_int))
