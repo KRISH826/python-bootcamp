96368617a8fa77b0f4200fd37e4ac99a2e77386f
KRISH826 <krishnendupanja98@gmail.com>
2026-06-25 00:25:38 +0530

    packages 2

--- imports-packaging/index.py
+++ imports-packaging/index.py
@@ -1,6 +1,7 @@
 # __package__
 from math import sqrt, pi, pow, floor
-from package.maths import addition
+from package.maths import *
+# from package.maths import addition
 import numpy as np
 
 numbers = 24587.58555
@@ -9,5 +10,8 @@ print(sqrt(4))
 print(pi)
 
 print(addition(2,4))
-
+print(substraction(4, 1))
 # numpy
+
+
+
